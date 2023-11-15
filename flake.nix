97470eb2826c300645ceef628f531ea489689e7c
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2023-11-15 21:04:56 +0100

    Build with Nix

--- flake.nix
+++ flake.nix
@@ -4,8 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    papermod = { url = "github:cydave/zola-theme-papermod"; flake = false; };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, papermod }:
     flake-utils.lib.eachDefaultSystem
     (
       system:
@@ -13,11 +14,27 @@
           pkgs = import nixpkgs {
             inherit system;
           };
+          themeName = ((builtins.fromTOML (builtins.readFile "${papermod}/theme.toml")).name);
         in
-        with pkgs;
         {
-          devShells.default = mkShell {
+          packages.website = pkgs.stdenv.mkDerivation rec {
+            name = "jsteuernagel-de-HEAD";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.zola ];
+            configurePhase = ''
+              mkdir -p "themes/${themeName}"
+              cp -r ${papermod}/* "themes/${themeName}"
+            '';
+            buildPhase = "zola build";
+            installPhase = "cp -r public $out";
+          };
+          defaultPackage = self.packages.${system}.website;
+          devShells.default = pkgs.mkShell {
             buildInputs = [ pkgs.zola ];
+            shellHook = ''
+              mkdir -p themes
+              ln -sn "${papermod}" "themes/${themeName}"
+            '';
           };
         }
     );
