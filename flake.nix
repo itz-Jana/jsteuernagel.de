fceb170168362c1e29970c82a9ec2811e6642b29
Jana Steuernagel <jana@jsteuernagel.de>
2024-09-25 09:23:44 +0200

    Nginx: Enable gzip

--- flake.nix
+++ flake.nix
@@ -41,6 +41,37 @@
               server {
                 listen [::]:${nginxPort} ipv6only=on;
                 index index.html;
+
+                gzip on;
+                gzip_disable "msie6";
+
+                gzip_vary on;
+                gzip_proxied any;
+                gzip_comp_level 6;
+                gzip_buffers 16 8k;
+                gzip_http_version 1.1;
+                gzip_min_length 256;
+                gzip_types
+                  application/atom+xml
+                  application/geo+json
+                  application/javascript
+                  application/x-javascript
+                  application/json
+                  application/ld+json
+                  application/manifest+json
+                  application/rdf+xml
+                  application/rss+xml
+                  application/xhtml+xml
+                  application/xml
+                  font/eot
+                  font/otf
+                  font/ttf
+                  image/svg+xml
+                  text/css
+                  text/javascript
+                  text/plain
+                  text/xml;
+
                 location / {
                   root ${website};
                 }
