01b46fbff18ffb4bfbefaafaf34df566a6d55be4
Jana Steuernagel <jana@jsteuernagel.de>
2024-10-08 15:12:27 +0200

    Add debugging shell

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
 
           dockerImage = pkgs.dockerTools.buildLayeredImage {
             name = "jsteuernagel-de";
-            contents = [ pkgs.dockerTools.fakeNss ];
+            contents = [ pkgs.dockerTools.fakeNss pkgs.dockerTools.binSh ];
             extraCommands = ''
               mkdir -p tmp/nginx_client_body
               mkdir -p var/log/nginx
